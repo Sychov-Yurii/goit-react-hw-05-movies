e1d6adcb4f5d3d070f635271958406e002df3173
Sychov-Yurii <sychov.yurii@outlook.com>
2023-12-22 13:27:16 +0100

    apdate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from '../pages/Home';
+import Cast from '../pages/Cast';
+import MovieDetails from '../pages/MovieDetails';
+import Movies from '../pages/Movies';
+import Reviews from '../pages/Reviews';
+import MoviesList from './MoviesList/MoviesList';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework 5
-    </div>
+    <>
+      <MoviesList />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cast" element={<Cast />} />
+        <Route path="/moviedetails" element={<MovieDetails />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="*" element={<h1>404</h1>} />
+      </Routes>
+    </>
   );
 };
---
+++ src/components/MoviesList/MoviesList.jsx
@@ -0,0 +1,23 @@
+import { NavLink } from 'react-router-dom';
+
+const MoviesList = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/movies">Movies</NavLink>
+          </li>
+          {/* <li>
+            <NavLink to="/moviedetails">Trending today</NavLink>
+          </li> */}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MoviesList;
