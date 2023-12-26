c62a9b9234a5023805a3c3d6caf5b5e6c1fc0bde
Sychov-Yurii <sychov.yurii@outlook.com>
2023-12-26 11:35:53 +0100

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,6 +16,7 @@ export const App = () => {
         <Route index element={<Home />} />
         <Route path="movies" element={<Movies />} />
         <Route path="movies/:movieId" element={<MovieDetails />} />
+        
         <Route path="movies/:movieId/cast" element={<Cast />} />
         <Route
           path="movies/:movieId/reviews"
