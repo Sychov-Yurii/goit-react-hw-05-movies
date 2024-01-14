50d0a156697a8a8c19ec424acdf2eb83a53214ac
Sychov-Yurii <sychov.yurii@outlook.com>
2024-01-14 16:01:33 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,37 +1,3 @@
-// import React, { lazy, Suspense } from 'react';
-// import { Route, Routes } from 'react-router-dom';
-// import Home from '../pages/Home';
-// import Cast from '../pages/Cast';
-// import MovieDetails from '../pages/MovieDetails';
-// import Movies from '../pages/Movies';
-// // import Reviews from '../pages/Reviews';
-// import Layout from '../Layout/Layout';
-
-// const Reviews = lazy(() => import('../pages/Reviews'));
-
-// export const App = () => {
-//   return (
-//     <Routes>
-//       <Route path="/" element={<Layout />}>
-//         <Route index element={<Home />} />
-//         <Route path="movies" element={<Movies />} />
-//         <Route path="movies/:movieId" element={<MovieDetails />}>
-//           <Route path="cast" element={<Cast />} />
-//         </Route>
-//         <Route
-//           path="movies/:movieId/reviews"
-//           element={
-//             <Suspense fallback={<>loading...</>}>
-//               <Reviews />
-//             </Suspense>
-//           }
-//         />
-//       </Route>
-//       <Route path="*" element={<h1>404</h1>} />
-//     </Routes>
-//   );
-// };
-
 import React, { Suspense, lazy, useState } from 'react';
 import { HashRouter, Route, Routes, Navigate, Link } from 'react-router-dom';
 import styled from 'styled-components';
