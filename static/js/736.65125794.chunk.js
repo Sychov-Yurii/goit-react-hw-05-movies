d3d99310af0d3ef3ff2364b0eb8035c593150845
Sychov-Yurii <125128096+Sychov-Yurii@users.noreply.github.com>
2024-01-14 14:59:41 +0000

    Deploying to gh-pages from @ Sychov-Yurii/goit-react-hw-05-movies@ebedc602e7b7af0dc74dc3f79a9256c989d68490 🚀

--- static/js/736.65125794.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[736],{736:function(e,t,r){r.r(t);r(791);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"Reviews"})}}}]);
-//# sourceMappingURL=736.65125794.chunk.js.map
